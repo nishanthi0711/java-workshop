a9e6ba070702174e39c8077e5de46ce5e4be8900
nishanthi0711 <nishaxaa1172@gmail.com>
2025-07-11 15:47:57 +0530

    sensor

--- src/com/vetias/java/workshop/temperaturedata/beans/Sensor.java
+++ src/com/vetias/java/workshop/temperaturedata/beans/Sensor.java
@@ -1,7 +1,46 @@
-package src.com.vetias.java.workshop.temperaturedata.beans;
+package com.vetias.java.workshop.temperaturedata.beans;
 
 public class Sensor {
-    private String name;
-    private int sensorid;
-    private double temperature;
+    private String id;
+    private String location;
+    private double value;
+
+    public Sensor(String id, String location, double value) {
+        this.id = id;
+        this.location = location;
+        this.value = value;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Sensor{" +
+                "id='" + id + '\'' +
+                ", location='" + location + '\'' +
+                ", value=" + value +
+                '}';
+    }
 }
